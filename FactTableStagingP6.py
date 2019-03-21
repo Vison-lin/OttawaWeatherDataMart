ce5c17348563dca98308496584529e13d26ec15a
Vison-lin <vison994@gmail.com>
2019-03-21 02:27:33 -0400

    new stage 6

--- FactTableStagingP6.py
+++ FactTableStagingP6.py
@@ -2,7 +2,7 @@ import csv
 
 
 
-need_index = [1,2,3,4,10]
+need_index = [1,2,3,4,10,15]
 
 
 collision_id =[]
@@ -25,12 +25,12 @@ def stagingP6_check_duplicate(filename):
     # print(len(collision_id))
 
 
-def stagingP6_create_table(filename,output):
+def stagingP6_check_fatal(filename,output):
     csvfile = open(filename + '.csv', 'r')
     collision_table = csv.reader(csvfile)
     final_table = []
     title =["Accident-key", "Location-key", "Hour-key", "Weather-key", "Event-key",
-                         "IMPACT_TYPE"]
+                         "IS_FATAL","IS_INTERSECTION"]
     final_table.append(title)
     count = 0
     for c in collision_table:
@@ -38,13 +38,19 @@ def stagingP6_create_table(filename,output):
         if c[0] in collision_id:
             result.append(count)
             count+=1
-            for index in range(1,14):
+            for index in range(1,15):
                 if index in need_index:
+                    if index == 10:
+                        check =False
+                        if c[index]=='Fatal injury':
+                            check = True
+                        result.append(check)
+                        continue
                     result.append(c[index])
             final_table.append(result)
 
     out_put_new(output,final_table)
-    print("stagingP6_create_table finish!!!")
+    print("stagingP6_check_fatal finish!!!")
 
 
 
@@ -59,5 +65,5 @@ def out_put_new(filename,list_data):
 
 def data_staging_phase_six(input, output):
     stagingP6_check_duplicate(input)
-    stagingP6_create_table(input, output)
+    stagingP6_check_fatal(input, output)
 
